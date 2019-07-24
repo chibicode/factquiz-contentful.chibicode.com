d773d503cd31b6c7c93be3c602c728d024ae7efd
Shu Uesugi <shu@chibicode.com>
2019-07-24 15:43:02 -0700

    Set up contentful

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const withCSS = require('@zeit/next-css')
 
-const config = withCSS()
+const config = withCSS({
+  env: {
+    contentfulSpaceId: 'rrjgm5kj5vfg',
+    contentfulDeliveryAPIToken: 'P-_zV0feS6OQA25F5hQQ6Kify9tT6RAAsKibGqNDLX4'
+  }
+})
 
 module.exports = config
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,19 @@
+/* eslint camelcase: ["error", {properties: "never"}] */
 import React from 'react'
+// import { createClient } from 'contentful'
 import Page from '../src/components/page'
 
-export default () => <Page />
+const Index = () => <Page />
+
+Index.getInitialProps = async () => {
+  // const client = createClient({
+  //   space: process.env.contentfulSpaceId,
+  //   accessToken: process.env.contentfulDeliveryAPIToken
+  // })
+
+  // TODO: Use query parameter to figure out which quiz to show
+  return {}
+}
+
+
+export default Index
